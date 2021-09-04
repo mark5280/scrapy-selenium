f2d5a89e944487dc093e9092bd326f9a8ec45d08
mark5280 <mark@rareearthapps.com>
2021-09-04 10:45:05 -0600

    Removed requirement for undetected-chromedriver

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup, find_packages
 
 setup(
     packages=find_packages(),
-    setup_requires=['scrapy', 'selenium', 'undetected-chromedriver']
+    setup_requires=['scrapy', 'selenium']
 )
